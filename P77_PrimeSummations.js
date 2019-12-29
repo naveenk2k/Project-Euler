ad09a15070e555fd18b85a2cf0f56967b56b5edf
Naveen K Murthy <naveenk2k@gmail.com>
2019-12-29 19:44:14 +0530

    Solved P77_PrimeSummations

--- P77_PrimeSummations.js
+++ P77_PrimeSummations.js
@@ -1,189 +1,62 @@
-/* Logic-
-Generate a bunch of primes.
-Create all combinations of their sums when taken 2 at a time, 3 at a time, 4 at a time, etc.
-When we generate any number by summing primes, increment the counter for the number of ways it's possible to form that number.
-The number which hits 500 first is our answer.
+/*
+Question:
+It is possible to write ten as the sum of primes in exactly five different ways:
 
-Need to debug because we're getting the wrong answer
+7 + 3
+5 + 5
+5 + 3 + 2
+3 + 3 + 2 + 2
+2 + 2 + 2 + 2 + 2
+
+What is the first value which can be written as the sum of primes in over five thousand different ways?
+
+Answer: 71 (~7ms)
 */
+
+// Logic: See the explanation for P76_CountingSummations and then read the changes below.
+
+console.time('Time');
 
 let primes = require('./Useful Programs/SieveOfEratosthenes')(500);
 
+// I'm considering the 0th prime as 0. No real reason to do this except that it sounds better now since the first prime, i.e primes[1], is 2, and so on.
+primes.unshift(0);
 
+const size = 10 ** 2;
+const target = 5000;
 
-let count = [];
+const solve = () => {
+    // Initializing the DP table with 'null's
+    const dp = Array(size).fill(0).map(x => Array(size).fill(null));
 
-function getAnswer() {
-    for (let i = 0; i < primes.length; i++) {
-        for (let j = i; j < primes.length; j++) {
-            let index = primes[i] + primes[j];
-            if (count[index] === undefined) {
-                count[index] = 0;
-            }
-            count[index]++;
-            if (count[index] >= 5000) {
-                console.log('FOUND at ' + index + count[index]);
+    // Filling the table with starting values:
+    // 1 way to make 0 using the 0th prime (=0) along with any other prime
+    for (let i = 0; i < size; i++)
+        dp[i][0] = 1;
 
-                return;
+    // 0 ways to make any number > 0 using just the 0th prime (=0).
+    // All even numbers can be made using the set {0, 2} but no odd number can.
+    for (let j = 1; j < size; j++) {
+        dp[0][j] = 0;
+        dp[1][j] = (j % 2 == 0) ? 1 : 0;
+    }
 
+    // Regular coin-change DP solution where the set of coins is replaced with the set of primes; hence the 'j-primes[i]' in the 'else' block.
+    // Also, I'm keeping track of the current minimum number that meets the target because the DP table is filled from left-to-right and then up-to-down.
+    let minimum = Infinity;
+    for (let i = 2; i < size; i++) {
+        for (let j = 1; j < size; j++) {
+            if (primes[i] > j) {
+                dp[i][j] = dp[i - 1][j];
+            } else {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - primes[i]];
+                if (dp[i][j] >= target && j < minimum)
+                    minimum = j;
             }
         }
     }
+    return minimum;
+};
 
-
-    for (let i = 0; i < primes.length; i++) {
-        for (let j = i; j < primes.length; j++) {
-            for (let k = j; k < primes.length; k++) {
-                let index = primes[i] + primes[j] + primes[k];
-                if (count[index] === undefined) {
-                    count[index] = 0;
-                }
-                count[index]++;
-                if (count[index] >= 5000) {
-                    console.log('FOUND at ' + index + count[index]);
-
-                    return;
-
-                }
-            }
-        }
-    }
-
-    for (let i = 0; i < primes.length; i++) {
-        for (let j = i; j < primes.length; j++) {
-            for (let k = j; k < primes.length; k++) {
-                for (let l = k; l < primes.length; l++) {
-                    let index = primes[i] + primes[j] + primes[k] + primes[l];
-                    if (count[index] === undefined) {
-                        count[index] = 0;
-                    }
-                    count[index]++;
-                    if (count[index] >= 5000) {
-                        console.log('FOUND at ' + index + count[index]);
-
-                        return;
-                    }
-                }
-            }
-        }
-    }
-
-    for (let i = 0; i < primes.length; i++) {
-        for (let j = i; j < primes.length; j++) {
-            for (let k = j; k < primes.length; k++) {
-                for (let l = k; l < primes.length; l++) {
-                    for (let m = l; m < primes.length; m++) {
-                        let index = primes[i] + primes[j] + primes[k] + primes[l] + primes[m];
-                        if (count[index] === undefined) {
-                            count[index] = 0;
-                        }
-                        count[index]++;
-                        if (count[index] >= 5000) {
-                            console.log('FOUND at ' + index + count[index]);
-
-
-                            return;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    for (let i = 0; i < primes.length; i++) {
-        for (let j = i; j < primes.length; j++) {
-            for (let k = j; k < primes.length; k++) {
-                for (let l = k; l < primes.length; l++) {
-                    for (let m = l; m < primes.length; m++) {
-                        for (let n = m; n < primes.length; n++) {
-                            let index = primes[i] + primes[j] + primes[k] + primes[l] + primes[m] + primes[n];
-                            if (count[index] === undefined) {
-                                count[index] = 0;
-                            }
-                            count[index]++;
-                            if (count[index] >= 5000) {
-                                console.log('FOUND at ' + index + count[index]);
-
-                                return;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    for (let i = 0; i < primes.length; i++) {
-        for (let j = i; j < primes.length; j++) {
-            for (let k = j; k < primes.length; k++) {
-                for (let l = k; l < primes.length; l++) {
-                    for (let m = l; m < primes.length; m++) {
-                        for (let n = m; n < primes.length; n++) {
-                            for (let o = n; o < primes.length; o++) {
-                                let index = primes[i] + primes[j] + primes[k] + primes[l] + primes[m] + primes[n] + primes[o];
-                                if (count[index] === undefined) {
-                                    count[index] = 0;
-                                }
-                                count[index]++;
-                                if (count[index] >= 5000) {
-                                    console.log('FOUND at ' + index + count[index]);
-                                    return;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-getAnswer();
-
-/*
-function isPrime(n) {
-    if (n <= 1) {
-        return false; //1 isn't prime
-    } else if (n == 2 || n == 3) {
-        return true;
-    } else if (n % 2 == 0 || n % 3 == 0) {
-        return false;
-    } else {
-        let limit = Math.sqrt(n);
-        for (let i = 3; i <= limit; i += 2) { //checking all numbers less than sqrt(n) for factors
-            if (n % i === 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
-
-//Logic is wrong - only counts sum of 2 individual numbers to give another number but this is not true: 10 = 2 + 2 + 2 + 2 + 2, 5 + 5, etc. The number of numbers is not known
-let ways = [0, 1, 1, 1];
-
-function getWays(n) {
-    if (ways[n]) {
-        return ways[n];
-    } else if (isPrime(n)) {
-        ways[n] = 1;
-        return 1;
-    } else {
-        let sum = 0;
-        for (let i = 1; i <= n / 2; i++) {
-            if (isPrime(i) && isPrime(n - i)) {
-                if (i != n - i) {
-                    sum += getWays(i) + getWays(n - i);
-                } else {
-                    sum += getWays(i);
-                }
-            }
-        }
-        ways[n] = sum;
-        return ways[n];
-    }
-}
-
-console.log(getWays(8));
-console.log(ways);
-*/
+console.log(solve());
+console.timeEnd('Time');
--- P78_CoinPartitions.js
+++ P78_CoinPartitions.js
@@ -1,9 +1,8 @@
 // DP doesn't work because the array size gets insanely large
 // Use Euler's formula
 
-console.time('Time');
-
-const size = 10 ** 4;
+const size = 10 ** 3;
+// const size = 6;
 
 const solve = size => {
     const dp = Array(size).fill(0).map(x => Array(size).fill(null));
@@ -19,10 +18,10 @@ const solve = size => {
             if (i > j)
                 dp[i][j] = dp[i - 1][j];
             else
-                dp[i][j] = (dp[i - 1][j] + dp[i][j - i]) % 10 ** 6;
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - i]);
 
-            if (dp[i][j] === 0) {
-                console.log(dp[i][j], j);
+            if (dp[i][j] % (10 ** 6) === 0) {
+                console.log('here', dp[i][j], j);
                 return [j];
             }
         }
@@ -34,4 +33,3 @@ const solve = size => {
 
 
 console.log(solve(size));
-console.timeEnd('Time');
